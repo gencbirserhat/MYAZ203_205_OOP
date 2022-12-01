542078df84f10e434fff9ba128fa549cecef1683
gencbirserhat <78283289+gencbirserhat@users.noreply.github.com>
2022-12-01 11:13:03 +0300

    a

---
+++ Week_10/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_10
+{
+    public interface IStudent
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } 
+        public string LastName { get; set; }
+        public string FullName(string firstName, string lastName);
+        public string Email(string fullname);
+    }
+}
---
+++ Week_10/StudentManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_10
+{
+    internal class StudentManager : IStudent 
+    {
+        private List<IStudent> _students;
+
+        public StudentManager()
+        {
+            _students = new List<IStudent>();
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email(string fullname)
+        {
+            return fullname + "@samsun.edu.tr";
+        }
+
+        public string FullName(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _students.GetEnumerator();
+        }
+    }
+}
